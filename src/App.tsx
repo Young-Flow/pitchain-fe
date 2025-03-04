6e793818d76bc99583690f6d259a0b19f6458b3b
ayden94 <144667387+ayden94@users.noreply.github.com>
2025-03-04 16:51:48 +0900

    feat : DefaultLayout 추가. DefaultLayout는 추후 로그인 로직이 정립되면 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,15 @@
+import { BrowserRouter, Route, Routes } from 'react-router';
+import Landing from './pages/Landing';
+import DefaultLayout from './widgets/default/DefaultLayout';
+
 export default function App() {
-  return <div className="h-10 w-10 bg-red-300"></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<DefaultLayout />}>
+          <Route index element={<Landing />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
