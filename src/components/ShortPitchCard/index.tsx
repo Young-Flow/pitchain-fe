e36e3d02d1ec146cb601f7d7e1bc51d3dc31d397
sangjin149 <sangjin149@gmail.com>
2025-03-05 22:25:05 +0900

    feat: 로고 작성 및 상수명 변경

--- src/components/ShortPitchCard/index.tsx
+++ src/components/ShortPitchCard/index.tsx
@@ -1,17 +1,28 @@
+import Avatar from '@components/Avatar';
+import clsx from 'clsx';
 import { ComponentPropsWithoutRef } from 'react';
 
-const ThumbnailSizeMap = new Map<string, { width: number; height: number }>([
+const THUMBNAIL_SIZE_MAP = new Map<string, { width: number; height: number }>([
   ['small', { width: 296, height: 167 }],
   ['medium', { width: 310, height: 175 }],
 ]);
 
 type ThumbnailProps = ComponentPropsWithoutRef<'img'> & {
+  src: string;
   size: 'small' | 'medium';
 };
 
-ShortPitchCard.Thumbnail = function ({ src, size, ...props }: ThumbnailProps) {
-  const sizeStyle = ThumbnailSizeMap.get(size);
-  return <img src={src} style={sizeStyle} alt="video card thumbnail" {...props} />;
+ShortPitchCard.Thumbnail = function ({ size, className, ...props }: ThumbnailProps) {
+  const SIZE_CLASSNAME = THUMBNAIL_SIZE_MAP.get(size);
+  return <img className={clsx(SIZE_CLASSNAME, className)} alt="video card thumbnail" {...props} />;
+};
+
+type LogoProps = ComponentPropsWithoutRef<'img'> & {
+  src: string;
+};
+
+ShortPitchCard.Logo = function ({ ...props }: LogoProps) {
+  return <Avatar size="medium" {...props} />;
 };
 
 export default function ShortPitchCard({ children, ...props }: ComponentPropsWithoutRef<'article'>) {
