4179e1677e0626545cd671418c18dcd94cd274f4
ayden94 <144667387+ayden94@users.noreply.github.com>
2025-03-06 18:24:00 +0900

    feat : 로그인/회원가입 공통 레이아웃 정리

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Landing from '@pages/Landing';
 import DefaultLayout from '@widgets/Layout/DefaultLayout';
 import SignLayout from '@widgets/Layout/SignLayout';
 import SocialLogin from '@pages/sign/SocialLogin';
+import SocialCallback from '@pages/sign/SocialCallback';
 
 export default function App() {
   return (
@@ -11,11 +12,16 @@ export default function App() {
         <Route path="/" element={<DefaultLayout />}>
           <Route index element={<Landing />} />
         </Route>
+
+        <Route path="/sign/:provider/callback" element={<SocialCallback />} />
+
         <Route path="/sign" element={<SignLayout />}>
           <Route path="inBusiness" />
           <Route path="upBusiness" />
           <Route path="SocialLogin" element={<SocialLogin />} />
         </Route>
+
+        <Route path="*" element={<div>404 Not Found</div>} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/widgets/Layout/SignLayout.tsx
@@ -0,0 +1,39 @@
+import { Link, Outlet, useLocation } from 'react-router';
+import Logo from '@components/Logo';
+import Clickable from '@components/Clickable/Clickable';
+
+export default function SignLayout() {
+  const { pathname } = useLocation();
+
+  return (
+    <main className="mx-auto flex w-424 flex-col items-center justify-center gap-36 p-12">
+      <Logo className="h-110 w-340" />
+
+      <div className="flex w-full flex-col gap-12">
+        <Outlet />
+
+        <div className="flex justify-center gap-8">
+          <Clickable shape="text" Component={Link} to={NAV_LINK[pathname][0].to} className="flex-0 text-nowrap">
+            {NAV_LINK[pathname][0].text}
+          </Clickable>
+
+          <div className="h-20 w-px bg-gray-400" />
+
+          <Clickable shape="text" Component={Link} to={NAV_LINK[pathname][1].to} className="flex-0 text-nowrap">
+            {NAV_LINK[pathname][1].text}
+          </Clickable>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+const SOCIAL_LOGIN_LINK = { text: '소셜 로그인', to: '/sign/socialLogin' };
+const BUSINESS_SIGNIN_LINK = { text: '기업 로그인', to: '/sign/inBusiness' };
+const BUSINESS_SIGNUP_LINK = { text: '기업 회원가입', to: '/sign/upBusiness' };
+
+const NAV_LINK: { [key: string]: Array<{ text: string; to: string }> } = {
+  '/sign/socialLogin': [BUSINESS_SIGNIN_LINK, BUSINESS_SIGNUP_LINK],
+  '/sign/inBusiness': [SOCIAL_LOGIN_LINK, BUSINESS_SIGNUP_LINK],
+  '/sign/upBusiness': [SOCIAL_LOGIN_LINK, BUSINESS_SIGNIN_LINK],
+};
