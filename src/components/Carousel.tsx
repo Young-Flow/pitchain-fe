c9fce65af4fd5607a76555af92063b3adce13cb3
sangjin149 <sangjin149@gmail.com>
2025-03-21 23:59:16 +0900

    feat: 좌클릭 버튼 컴포넌트 구현

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,7 +1,30 @@
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import Slider, { Settings } from 'react-slick';
+import { createContext, useContext, useRef, RefObject, ComponentProps } from 'react';
+import Clickable from '@components/Clickable/Clickable';
 
+const sliderRefContext = createContext<RefObject<Slider | null> | null>(null);
+
+//TODO: 버튼 배경, 버튼 컴포넌트 작성
+//TODO: Nav 컴포넌트 작성
 export default function Carousel({ ...props }: Settings) {
-  return <Slider {...props} />;
+  const sliderRef = useRef<Slider | null>(null);
+
+  return (
+    <sliderRefContext.Provider value={sliderRef}>
+      <div>
+        <Slider {...props} />
+      </div>
+    </sliderRefContext.Provider>
+  );
 }
+
+Carousel.LeftButton = function LeftButton({ children, ...props }: ComponentProps<'button'>) {
+  const sliderRef = useContext(sliderRefContext);
+  function handleClick() {
+    if (sliderRef === null) return;
+    sliderRef.current.slickPrev();
+  }
+  return <Clickable {...props}>{children}</Clickable>;
+};
--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,3 +1,9 @@
+import Carousel from '@components/Carousel';
+
 export default function Landing() {
-  return <></>;
+  return (
+    <div className="w-1300">
+      <Carousel />
+    </div>
+  );
 }
